83647849f4165b05ee0e8b5eb6cc9f92f6c18a68
gaetsd <gaetan.deshayes2@gmail.com>
2016-02-11 20:19:46 +0100

    distance

--- src/POC/Position.java
+++ src/POC/Position.java
@@ -13,4 +13,10 @@ public class Position {
         Row = row;
         Column = column;
     }
+
+    public static double distance(Position p1, Position p2){
+    	double distX = Math.abs((double)p1.Row - (double)p2.Row);
+    	double distY = Math.abs((double)p1.Column - (double)p2.Column);
+    	return Math.ceil(Math.sqrt(distX*distX + distY*distY));
+    }
 }
