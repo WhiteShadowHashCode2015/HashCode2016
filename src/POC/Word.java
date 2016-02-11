899a6954225a97674b610554d923eb8adf3a7a74
KLAM Alexis <alexisk@exakis.com>
2016-02-11 19:18:18 +0100

    add Model

---
+++ src/POC/Word.java
@@ -0,0 +1,18 @@
+package POC;
+
+import java.util.List;
+
+/**
+ * Created by alexisk on 11/02/2016.
+ */
+public class Word {
+
+    public List<Warehouse> WareHouses;
+
+    public List<Drone>  Drones;
+
+    public int CurrentRound;
+
+    public int MaxRound;
+
+}
